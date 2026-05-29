2e0170fa86098309bab0ad3394660eb34dec9b44
mosfiqtonoy <mosfiqtonoy@gmail.com>
2026-05-29 17:13:17 +0600

    Create __init__.py

---
+++ models/__init__.py
@@ -0,0 +1,11 @@
+# models/__init__.py
+"""
+Models Package Initializer.
+Exposes core structural class definitions globally for cleaner import operations.
+"""
+
+from .user import User
+
+__all__ = [
+    'User'
+]
