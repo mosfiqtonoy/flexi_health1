cd8cd3eb7e80f2282b9a72d53405f41e3ac3282f
mosfiqtonoy <mosfiqtonoy@gmail.com>
2026-06-07 22:49:10 +0600

    Create main.dart

---
+++ mobile_app/lib/main.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'FlexiHealth',
+      theme: ThemeData(primarySwatch: Colors.blue),
+      home: const Scaffold(body: Center(child: Text("Welcome to FlexiHealth!"))),
+    );
+  }
+}
