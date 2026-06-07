6754618a598012df9390c67bc0d22f4e4bc9e087
mosfiqtonoy <mosfiqtonoy@gmail.com>
2026-06-07 22:51:10 +0600

    Update api_service.dart

--- mobile_app/lib/services/api_service.dart
+++ mobile_app/lib/services/api_service.dart
@@ -1,1 +1,22 @@
+// lib/services/api_service.dart
+import 'dart:convert';
+import 'package:http/http.dart' as http;
 
+class ApiService {
+ 
+  final String baseUrl = "http://10.0.2.2:5000/api/v1";
+
+  Future<Map<String, dynamic>> loginUser(String email, String password) async {
+    final response = await http.post(
+      Uri.parse('$baseUrl/auth/login'),
+      headers: {"Content-Type": "application/json"},
+      body: jsonEncode({"email": email, "password": password}),
+    );
+
+    if (response.statusCode == 200) {
+      return jsonDecode(response.body);
+    } else {
+      throw Exception("Login Failed: ${response.statusCode}");
+    }
+  }
+}
